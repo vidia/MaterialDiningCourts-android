a2573b0480c0cc0a284395d51aaf41de9c2f1765
David Tschida <dmtschida1@gmail.com>
2015-12-01 19:08:46 -0500

    Attempt to save favorites data to an anon user locally
    Goal was to save anon locally then when the user signs in this gets uploaded.

--- app/src/main/java/com/davidtschida/materialdiningcourts/adapters/FoodItemsAdapter.java
+++ app/src/main/java/com/davidtschida/materialdiningcourts/adapters/FoodItemsAdapter.java
@@ -9,8 +9,12 @@ import android.widget.TextView;
 
 import com.davidtschida.materialdiningcourts.R;
 import com.davidtschida.purduemenu.models.FoodItem;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 import java.util.List;
+
+import timber.log.Timber;
 
 /**
  * Created by david on 11/3/2015.
@@ -39,11 +43,26 @@ public class FoodItemsAdapter extends RecyclerView.Adapter<FoodItemsAdapter.View
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, final int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
         holder.mRootView.setBackgroundColor((position % 2 == 0) ? Color.WHITE : Color.LTGRAY);
         holder.mTextView.setText(mFoodsList.get(position).getName());
+        holder.mFavoritesHeart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Timber.d("A heart was tapped!!!");
+                FoodItem foodItem = mFoodsList.get(position);
+                ParseObject favorite = new ParseObject("favorite");
+                favorite.add("foodItemId", foodItem.getId());
+                ParseUser.getCurrentUser().addUnique("favorites", favorite);
+                favorite.saveInBackground();
+                ParseUser.getCurrentUser().saveInBackground();
+
+                Timber.d(favorite.get("foodItemId").toString() + " " + foodItem.getId());
+                Timber.d(ParseUser.getCurrentUser().toString());
+            }
+        });
     }
 
     // Return the size of your dataset (invoked by the layout manager)
@@ -56,13 +75,16 @@ public class FoodItemsAdapter extends RecyclerView.Adapter<FoodItemsAdapter.View
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
+
         // each data item is just a string in this case
         public TextView mTextView;
         public View mRootView;
+        public TextView mFavoritesHeart;
 
         public ViewHolder(View parent) {
             super(parent);
             mTextView = (TextView) parent.findViewById(R.id.simpleItemTextView);
+            mFavoritesHeart = (TextView) parent.findViewById(R.id.favoritesHeart);
             mRootView = parent;
         }
     }
