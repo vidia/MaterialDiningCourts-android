1c8a74b3a0643f9783a576ce05c70dbb2112b929
David Tschida <dmtschida1@gmail.com>
2015-12-04 17:13:05 -0500

    Added Observable versions for Retrofit

--- app/src/main/java/com/davidtschida/purduemenu/MenuService.java
+++ app/src/main/java/com/davidtschida/purduemenu/MenuService.java
@@ -9,6 +9,7 @@ import com.davidtschida.purduemenu.models.SearchResults;
 import retrofit.Call;
 import retrofit.http.GET;
 import retrofit.http.Path;
+import rx.Observable;
 
 /**
  * Created by david on 10/20/2015.
@@ -35,4 +36,30 @@ public interface MenuService {
 
     @GET("/items/{guid}/schedule/")
     Call<FoodItemSchedule> getItemSchedule(@Path("guid") String guid);
+
+    /*
+     * Observable versions for RxJava
+     */
+
+    @GET("locations")
+    Observable<Locations> getDiningLocationsObservable();
+
+    //@GET("/retail/")
+    //Call<List<RetailLocation>> getRetailLocationsObservable();
+
+    @GET("locations/{location}/{date}")
+        //"mm-dd-yyyy"
+    Observable<DayMenu> getDiningMenuObservable(@Path("location") String location, @Path("date") String date);
+
+    @GET("/items/search/{terms}/")
+    Observable<SearchResults> searchItemsObservable(@Path("terms") String searchTerms);
+
+    @GET("/items/searchUpcoming/{terms}/")
+    Observable<SearchResults> searchUpcomingItemsObservable(@Path("terms") String searchTerms);
+
+    @GET("/items/{guid}/")
+    Observable<FoodItem> getItemObservable(@Path("guid") String guid);
+
+    @GET("/items/{guid}/schedule/")
+    Observable<FoodItemSchedule> getItemScheduleObservable(@Path("guid") String guid);
 }
